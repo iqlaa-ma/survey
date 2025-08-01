abc27de16087928caf59e9600d54ad29ef8b9816
Ayoub Elidryssy <aelidrys@c4r9p4.1337.ma>
2025-08-01 10:55:12 +0100

    update-ui

--- src/app/responses/page.tsx
+++ src/app/responses/page.tsx
@@ -102,24 +102,25 @@ const Responses: React.FC = () => {
 
   return (
     <div className='bg-gray-800 flex flex-col items-center justify-center' dir="rtl">
-      <h2 className='mt-10 mb-2 text-4xl font-bold text-center mb-10' style={{color: "cyan"}}>{totalResponses}</h2>
+      <h2 className='mt-10 text-4xl font-bold text-center mb-10' style={{color: "cyan"}}>{totalResponses}</h2>
       <ul className='w-[50%] flex flex-col gap-8 mb-50' style={{ minWidth: 300 }}>
         {_questions.filter(q => q.type === 'multiple_choice').map(q => (
-          <li key={q.id} className='bg-gray-700 rounded-lg p-3' style={{}} >
-            <div className='font-bold mb-8' style={{color: "#e7e7e7ff", fontSize: "20px"}}>{q.question}</div>
-            <ul className=' flex flex-col gap-2'>
+          <li key={q.id} className='bg-gray-700 rounded-lg' style={{}} >
+            <div className='font-bold mb-4 bg-gray-600 p-3 pr-6 rounded-t-lg' style={{color: "#e7e7e7ff", fontSize: "20px"}}>{q.question}</div>
+            <ul className=' flex flex-col gap-2 p-3'>
               {q.options && [...q.options]
                 .sort((a, b) => (stats[q.id]?.[b] ?? 0) - (stats[q.id]?.[a] ?? 0))
-                .map(option => {
+                .map((option, index) => {
                 const count = stats[q.id]?.[option] ?? 0;
                 const total: any = Object.values(stats[q.id] || {}).reduce((a, b) => Number(a) + Number(b), 0);
                 const percent = total > 0 ? (count / total) * 100 : 0;
+                let bg_color = ["bg-orange-900","bg-blue-900", "bg-green-900"] 
 
                 return (
-                  <li key={option} className='mb-1'>
-                    <div className='relative h-full w-full rounded-lg bg-green-900' style={{zIndex: '0', height: '2.75rem'}} >
+                  <li key={option} className=''>
+                    <div className={`relative h-full w-full rounded-lg ${bg_color[index]}`} style={{zIndex: '0', height: '2.75rem'}} >
                       <div className='absolute top-0 h-full rounded-lg bg-green-700'
-                        style={{width: `${percent}%`, zIndex: 1, transition: 'width 0.5s', background: "#6b6b6b7e"}} />
+                        style={{width: `${percent}%`, zIndex: 1, transition: 'width 0.5s', background: "rgb(157 155 155 / 49%)"}} />
 
                       <div className='relative z-10 p-4 flex items-center h-full w-full justify-between' style={{color: "white"}}>
                         <span className='font-bold'>{option}</span>
